6f086c70b08158c6181f5ae921cca4f0b65e3248
Kevin Tavukciyan <kevintavukciyan@gmail.com>
2019-06-03 14:49:52 +0200

    Iterator: rewrote the base of the class
    Signed-off-by: Kevin Tavukciyan <kevintavukciyan@gmail.com>

--- include/Core/Iterator/Iterator.hh
+++ include/Core/Iterator/Iterator.hh
@@ -1,121 +1,26 @@
 #pragma once
-
 #include "CFG.hh"
 #include "Func.hh"
-#include <llvm/IR/Instructions.h>
+#include "Intervals.hh"
+#include <llvm/IR/Instruction.h>
 
-/// Iterator class: This class is used to abstract the cfg with
-/// the wanted domain.
-/// @param T: Domain class name
-/// @param V: Abstract Value type in domain class (ex:
-/// IntervalDomain::AbstractValue)
-
-template <typename T, typename V> class Iterator {
+/// @brief Iterator class: This class is used to abstract the
+/// cfg with the specified domain.
+/// @tparam T: Domain name
+template <typename T> class Iterator {
 public:
   Iterator(const CFG &cfg);
-  T &operator()(T &dom);
-  T cfg_to_dom();
+  T compute_dom_from_cfg();
 
 private:
-  void set_init_worklist(std::shared_ptr<Func>, T &dom);
-  void eval_stat(V env, llvm::Instruction *inst, T &dom);
-  void invariant_propagation();
+  void init_worklist(std::shared_ptr<Func> entry);
+  void compute_abstract_domain();
+  void eval_stat(llvm::Instruction *inst);
+
+private:
+  std::vector<std::shared_ptr<Node>> _worklist;
+  T _dom;
   const CFG &_cfg;
-  std::vector<std::shared_ptr<Node>> worklist;
 };
 
-template <typename T, typename V>
-Iterator<T, V>::Iterator(const CFG &cfg) : _cfg(cfg) {}
-
-// I'm forcing the analysis on main for now. Will change in the future
-template <typename T, typename V> T &Iterator<T, V>::operator()(T &dom) {
-  std::shared_ptr<Func> func;
-
-  for (auto &A : _cfg.get_cfg_funcs())
-    if (A->name.find("main") != std::string::npos) {
-      func = A;
-      break;
-    }
-
-  std::cout << (func ? func->name : "empty") << "\n";
-
-  if (func != nullptr) {
-    set_init_worklist(func, dom);
-  }
-  invariant_propagation();
-  return dom;
-}
-
-template <typename T, typename V>
-void Iterator<T, V>::set_init_worklist(std::shared_ptr<Func> func, T &dom) {
-  if (func == nullptr)
-    return;
-
-  std::shared_ptr<Node> entry_node = func->func_entry;
-  std::set<std::shared_ptr<Node>> passed;
-  std::vector<std::shared_ptr<Node>> stack;
-  for (auto &A : func->args) {
-    auto env = dom.init();
-    if (auto retval = llvm::dyn_cast<llvm::Value>(A))
-      dom.add_var(env, retval->getName());
-  }
-
-  while ((entry_node->arc_out.empty() == false) ||
-         ((passed.size() < func->bb_cnt) || (stack.empty() == false))) {
-    if (entry_node->label != "")
-      passed.insert(entry_node);
-
-    worklist.push_back(entry_node);
-    for (auto &A : entry_node->arc_out) {
-      if (llvm::isa<llvm::ReturnInst>(A->inst) == false &&
-          llvm::isa<llvm::StoreInst>(A->inst) == false) {
-        eval_stat(dom.init(), A->inst, dom);
-      }
-      stack.push_back(A->node_out);
-    }
-
-    if (stack.size() == 0)
-      return;
-
-    do {
-      entry_node = stack.back();
-      stack.pop_back();
-    } while ((passed.count(entry_node)) && stack.size() > 0);
-
-    if (passed.count(entry_node) && stack.size() == 0)
-      return;
-  }
-}
-
-// prepare for the if forest :)
-template <typename T, typename V>
-void Iterator<T, V>::eval_stat(V env, llvm::Instruction *inst, T &dom) {
-  if (llvm::isa<llvm::AllocaInst>(inst)) {
-    if (auto retval = llvm::dyn_cast<llvm::Value>(inst)) {
-      dom.add_var(env, retval->getName());
-      llvm::errs() << *retval->getType() << "\n";
-    }
-  }
-  if (llvm::isa<llvm::GetElementPtrInst>(inst))
-    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
-      dom.add_var(env, retval->getName());
-
-  if (llvm::isa<llvm::LoadInst>(inst))
-    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
-      dom.add_var(env, retval->getName());
-
-  if (llvm::isa<llvm::BinaryOperator>(inst))
-    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
-      dom.add_var(env, retval->getName());
-
-  if (llvm::isa<llvm::PtrToIntInst>(inst))
-    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
-      dom.add_var(env, retval->getName());
-
-  if (llvm::isa<llvm::IntToPtrInst>(inst))
-    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
-      dom.add_var(env, retval->getName());
-}
-
-template <typename T, typename V>
-void Iterator<T, V>::invariant_propagation() {}
+#include "Iterator.hxx"
---
+++ include/Core/Iterator/Iterator.hxx
@@ -0,0 +1,156 @@
+#pragma once
+#include "Iterator.hh"
+
+template <typename T>
+Iterator<T>::Iterator(const CFG &cfg) : _cfg(cfg){
+  auto domain = T();
+  _dom = domain;
+}
+
+template <typename T>
+void Iterator<T>::init_worklist(std::shared_ptr<Func> entry)
+{
+  if (entry == nullptr)
+    return;
+
+  std::shared_ptr<Node> entry_node = entry->func_entry;
+  std::set<std::shared_ptr<Node>> passed;
+  std::vector<std::shared_ptr<Node>> stack;
+
+  for (auto &A : entry->args)
+  {
+    if (auto retval = llvm::dyn_cast<llvm::Value>(A))
+      _dom.update_env(retval->getName().str(), _dom.init_abs_val());
+  }
+
+  while ((entry_node->arc_out.empty() == false) ||
+         ((passed.size() < entry->bb_cnt) || (stack.empty() == false))) {
+    if (entry_node->label != "")
+      passed.insert(entry_node);
+
+    _worklist.push_back(entry_node);
+    for (auto &A : entry_node->arc_out) {
+      if (llvm::isa<llvm::ReturnInst>(A->inst) == false &&
+          llvm::isa<llvm::StoreInst>(A->inst) == false) {
+        eval_stat(A->inst);
+      }
+      stack.push_back(A->node_out);
+    }
+
+    if (stack.size() == 0)
+      return;
+
+    do {
+      entry_node = stack.back();
+      stack.pop_back();
+    } while ((passed.count(entry_node)) && stack.size() > 0);
+
+    if (passed.count(entry_node) && stack.size() == 0)
+      return;
+  }
+  for (auto &V : _worklist)
+    std::cout << V->id << "\n";
+}
+
+// Doing this because we have two other choices: (hate it though)
+// either do a switch case on every single IR instructions/opcodes
+// or do a specialized visitorInst class. The first one is same/worse than
+// the if forest below. The second idea is really not that
+// useful for what i am doing and at this point, adds more work
+// and OO techniques that are useless for my usecase. If in the
+// future someone sees this and has a better idea, i'd love
+// to know it, please drop an issue on github :)
+template <typename T>
+void Iterator<T>::eval_stat(llvm::Instruction *inst)
+{
+  if (llvm::isa<llvm::AllocaInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+      _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::GetElementPtrInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+      _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::LoadInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+      _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::BinaryOperator>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+      _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::PtrToIntInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+      _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::IntToPtrInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+      _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::CatchSwitchInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::InvokeInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::TruncInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::ZExtInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::SExtInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::FPToUIInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::FPToSIInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::UIToFPInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::SIToFPInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::FPTruncInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::FPExtInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::BitCastInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::VAArgInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::CleanupPadInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::CatchPadInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::ICmpInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::FCmpInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::PHINode>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::SelectInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::ExtractElementInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::ShuffleVectorInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::ExtractValueInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::InsertElementInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+  if (llvm::isa<llvm::InsertValueInst>(inst))
+    if (auto retval = llvm::dyn_cast<llvm::Value>(inst))
+    _dom.update_env(retval->getName(), _dom.init_abs_val());
+}
